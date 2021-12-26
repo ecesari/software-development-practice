2fa4f3255513352305ffbbbbe23849dfca4e4975
Ece <Ece@DESKTOP>
2021-12-27 00:52:35 +0300

    logo update

--- frontend/src/layout/AppHeader.vue
+++ frontend/src/layout/AppHeader.vue
@@ -2,7 +2,7 @@
   <header class="header-global">
     <base-nav class="navbar-main" transparent type="" effect="light" expand>
       <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-        <img src="img/brand/white.png" alt="logo" />
+        <img style="height='45px !important'" src="img/brand/foo.png" alt="logo" />
       </router-link>
 
       <div class="row" slot="content-header" slot-scope="{ closeMenu }">
